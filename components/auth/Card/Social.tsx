a0621bf3d871aa72c1ce3ed3a71d6a16bbf54a3c
Orlinside <orlinside.media@gmail.com>
2025-06-02 17:40:32 +0200

    Update Social.tsx

--- components/auth/Card/Social.tsx
+++ components/auth/Card/Social.tsx
@@ -14,7 +14,7 @@ export function Social() {
   return (
     <div className="flex items-center w-full gap-x-2">
       <button
-        className="w-full h-[40px] flex justify-center items-center bg-gray-300 p-2 rounded-xl hover:bg-slate-300 transition-all ease-in-out duration-200"
+        className="w-full h-[35px] flex justify-center items-center bg-dark p-2 rounded-[5px] hover:bg-dark/60 transition-all ease-in-out duration-200"
         onClick={() => onClick("google")}
       >
         <FcGoogle className="h-5 w-5" />
