3a9c1a59078fc8aca3ba519685842be5aefb99bf
Orlinside <orlinside.media@gmail.com>
2025-06-02 17:37:20 +0200

    Update FormSucess.tsx

--- components/auth/Card/FormSucess.tsx
+++ components/auth/Card/FormSucess.tsx
@@ -8,7 +8,7 @@ export const FormSuccess = ({ message }: FormSuccessProps) => {
   if (!message) return null;
 
   return (
-    <div className=" bg-gradient 5 p-3 rounded-xl uppercase flex items-center justify-center gap-x-2 text-sm text-emerald-500">
+    <div className=" bg-gradient 5 p-2 rounded-xl uppercase flex items-center justify-center gap-x-2 text-xs text-emerald-500">
       <p className="">{message}</p>
     </div>
   );
